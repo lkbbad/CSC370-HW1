906ce09e2fb7e29b09fa9f281b885c58b2d5e51a
Lindy Bustabad <libustabad@davidson.edu>
2020-08-31 15:40:14 -0400

    h1 started

--- a_star.py
+++ a_star.py
@@ -1,6 +1,19 @@
 import eightpuzzle
 
+def h1(initState):
+    h1 = 0
+    count = 0
+    for i in range(0,3):
+        for j in range(0,3):
+            if initState.state[i][j] != count:
+                # print(initState.state[i][j], count)
+                if count != 0:
+                    h1 += 1
+            count += 1
+    return h1
+
 if __name__ == '__main__':
     puzzle = eightpuzzle.EightPuzzle()
     initState = eightpuzzle.initTiles(puzzle)
-    # print(initState)
+    print(initState)
+    print(h1(initState))
