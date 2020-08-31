7dd3aec9f6367a2e9ca1e9486ab2c0f58ec3bfab
Lindy Bustabad <libustabad@davidson.edu>
2020-08-31 13:31:37 -0400

    Beginning of randomizing walks for tiles.

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,11 +1,14 @@
 import sys
 import copy
+import math
+import random
 
 class EightPuzzle():
     def __init__(self):
         #self.state = [[[0], [1], [2]], [[3], [4], [5]], [[6], [7], [8]]]
-        self.state = [[3, 1, 2], [4, 0, 5], [6, 7, 8]]
-        self.blank_index = self.get_blank_index()
+        self.state = [[1, 2, 0], [3, 4, 5], [6, 7, 8]]
+        self.x = self.get_blank_index()[0]
+        self.y = self.get_blank_index()[1]
 
     def get_blank_index(self):
         for i in range(0, 3):
@@ -17,9 +20,11 @@ class EightPuzzle():
 
     def neighbors(self):
         list = []
-        blank = self.get_blank_index()
-        x = blank[0]
-        y = blank[1]
+        # blank = self.get_blank_index()
+        # x = blank[0]
+        # y = blank[1]
+        x = self.x
+        y = self.y
         if x > 0:
             r = copy.deepcopy(self) # make a copy of state
             r.state[y][x] = r.state[y][x-1] # move right the tile to the left of space
@@ -37,21 +42,33 @@ class EightPuzzle():
             list.append((d,'d')) # add this move and board state to list
         if y < 2:
             u = copy.deepcopy(self) # make a copy of state
-            u.state[y][x] = u.state[y+1][x] # move tile below spac up
+            u.state[y][x] = u.state[y+1][x] # move tile below space up
             u.state[y+1][x] = 0
             list.append((u,'u')) # add this move and board state to list
-        print(list)
         return list
+
+    def initTiles(self):
+        direction = 0
+        # blank = self.get_blank_index()
+        # x = blank[0]
+        # y = blank[1]
+        print(self.state)
+        for i in range(0, 1):
+            direction = random.randrange(0, 2, 1)
+            if (direction == 0 and self.x > 0):
+                self.state[self.y][self.x] = self.state[self.y][self.x-1] # move right the tile to the left of space
+                self.state[self.y][self.x-1] = 0
+            print(self.state)
+            elif (direction == 1 and self.y > 0):
+                self.state[self.y][self.x] = self.state[self.y][self.x+1] # move right the tile to the left of space
+                self.state[self.y][self.x+1] = 0
+            print(self.state)
 
 
 if __name__ == '__main__':
    puzzle = EightPuzzle()
-   print(puzzle.blank_index)
+   print(puzzle.x)
+   print(puzzle.y)
    puzzle.neighbors()
-#    for i in range(0,3):
-#        for j in range(0,3):
-#             if puzzle[i][j] == 0:
-#                 x = j
-#                 y = i
-#     print((x,y))
+   puzzle.initTiles()
     
