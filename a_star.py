7e41ee98c7fba21af7914535c943475cae1e3e52
Your Name <casigl@davidson.edu>
2020-09-03 11:57:25 -0400

    implemented a_star for h2

--- a_star.py
+++ a_star.py
@@ -16,12 +16,12 @@ def h1(curr_state,prev_state, goal_state):
     
     curr_state.h = h1
     
+    #making sure heuristic is consistent
+    #never enters this??
     if (not(prev_state.h - curr_state.h <= curr_state.g)):
         curr_state.h = prev_state.h - curr_state.g
         h1 = prev_state.h - curr_state.g
-        
-    curr_state.h = h1
-    
+            
     return h1
 
 def goal_positions(tile):
@@ -31,83 +31,94 @@ def goal_positions(tile):
             if goal_state[i][j] == tile:
                 return (i,j)
 
-def h2(initState):
+def h2(curr_state,prev_state, goal_state):
+
     h2 = 0
     for i in range(0,3):
         for j in range(0,3):
-            goal_pos = goal_positions(initState.state[i][j])
-            # print("Goal: " + str(goal_pos))
-            # print("Current: " + str((i,j)))
-            res = tuple(map(lambda i, j: i - j, goal_pos, (i,j)))
-            # print(res)
-            res_x = abs(res[0])
-            res_y = abs(res[1])
-            if initState.state[i][j] != 0:
-                manhattan_distance = res_x + res_y
-            else:
-                manhattan_distance = 0
-            # print(manhattan_distance)
+            curr_pos = (i,j) #coordinates of current tile
+            goal_pos = goal_positions(curr_state.state[i][j]) #coordinates of current tile in goal board
+            manhattan_distance = abs(curr_pos[0] - goal_pos[0]) + abs(curr_pos[1] - goal_pos[1])
+            
             h2 += manhattan_distance
+        
+    curr_state.h = h2
+    
+    #making sure heuristic is consistent
+    #never enters this??
+    if (not(prev_state.h - curr_state.h <= curr_state.g)):
+        curr_state.h = prev_state.h - curr_state.g
+        h2 = prev_state.h - curr_state.g
     return h2
     
-    
+
 
 if __name__ == '__main__':
     puzzle = eightpuzzle.EightPuzzle()
     initState = eightpuzzle.initTiles(puzzle)
     print(initState.state)
+    #Running a_star with both h1 and h2
+    heuristics = [h1, h2]
     
-    #Initializing PriorityQueue frontier with neighbors
-    frontier = PriorityQueue()
+    for heuristic in heuristics:
     
-    frontier.put((0, initState))
+    
+        #Initializing PriorityQueue frontier with neighbors
+        frontier = PriorityQueue()
         
-    empty = False
-    goal_found = False
-    nodes_visited = 0
-    
-    goal = eightpuzzle.EightPuzzle()
-    explored_set = set()
+        frontier.put((0, initState))
         
-    while(not(empty) and not(goal_found)):
-        empty = frontier.empty()
-        if (empty):
-            break
-        if (nodes_visited > 0):
-            prev_node = next_node
+        empty = False
+        goal_found = False
+        nodes_visited = 0
+        
+        #Establishing the goal state
+        goal = eightpuzzle.EightPuzzle()
+        
+        #Creating any empty set to keep track of visited nodes
+        explored_set = set()
             
-        next = frontier.get()
-        next_node = next[1]
-        if next_node in explored_set:
-            explored = True
-            while(explored):
-                next = frontier.get()
-                next_node = next[1]
-                if not(next_node in explored_set):
-                    explored = False
+        #Run a_star while frontier is full and goal hasn't been found
+        while(not(empty) and not(goal_found)):
+            empty = frontier.empty()
+            if (empty):
+                break
+            if (nodes_visited > 0):
+                prev_node = next_node
                 
-                
-        explored_set.add(next_node)
+            next = frontier.get()
+            next_node = next[1]
+            #only add popped node to frontier if not yet explored
+            if next_node in explored_set:
+                explored = True
+                while(explored):
+                    next = frontier.get()
+                    next_node = next[1]
+                    if not(next_node in explored_set):
+                        explored = False
+                    
+            #add non-eplored node to explored set
+            explored_set.add(next_node)
+            
+            if (nodes_visited == 0):
+                prev_node = next_node
+            
+            print(next_node)
+            
+            nodes_visited += 1
+            if (next_node.state == goal.state):
+                goal_found = True
+                break
+            neighbors = next_node.neighbors()
+            for moves in neighbors:
+                frontier.put((heuristic(moves[0], prev_node, goal) + moves[0].g, moves[0]))
+            
+        if (empty):
+            print("No possible solutions using", heuristic, "as the heuristic.")
         
-        if (nodes_visited == 0):
-            prev_node = next_node
-        
-        print(next_node)
-        
-        nodes_visited += 1
-        if (next_node.state == goal.state):
-            goal_found = True
-            break
-        neighbors = next_node.neighbors()
-        for moves in neighbors:
-            frontier.put((h1(moves[0], prev_node, goal) + moves[0].g, moves[0]))
-        
-    if (empty):
-        print("No possible solutions :(")
-    
-    if (goal_found):
-        print("Found a solution!")
-        print("-----------------")
-        print("Nodes visited: ", nodes_visited)
-        print("Depth: ", next_node.g)
-        print("-----------------")
+        if (goal_found):
+            print("Found a solution using ", heuristic, "as the heuristic!")
+            print("-----------------")
+            print("Nodes visited: ", nodes_visited)
+            print("Depth: ", next_node.g)
+            print("-----------------")
