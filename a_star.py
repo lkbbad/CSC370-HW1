2b5bfa3e7a6d059f0f4adbbf21123442043d5bfc
Your Name <casigl@davidson.edu>
2020-09-07 15:11:18 -0400

    fixed heuristics

--- a_star.py
+++ a_star.py
@@ -9,13 +9,13 @@ import pandas
 NUMBER_OF_TRIALS = 50
 goal_state = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 
-def h1(curr_state,prev_state, goal_state):
+def h1(curr_state, goal_state):
     
     h1 = 0
     count = 0
     for i in range(0,3):
         for j in range(0,3):
-            if (not(curr_state.state[i][j] == goal_state.state[i][j])):
+            if (not(curr_state.state[i][j] == goal_state.state[i][j]) and not(curr_state.state[i][j] == 0)):
                 h1 += 1
     
     curr_state.h = h1
@@ -29,16 +29,17 @@ def goal_positions(tile):
             if goal_state[i][j] == tile:
                 return (i,j)
 
-def h2(curr_state,prev_state, goal_state):
+def h2(curr_state, goal_state):
 
     h2 = 0
     for i in range(0,3):
         for j in range(0,3):
-            curr_pos = (i,j) #coordinates of current tile
-            goal_pos = goal_positions(curr_state.state[i][j]) #coordinates of current tile in goal board
-            manhattan_distance = abs(curr_pos[0] - goal_pos[0]) + abs(curr_pos[1] - goal_pos[1])
+            if(not(curr_state.state[i][j] == 0)):
+                curr_pos = (i,j) #coordinates of current tile
+                goal_pos = goal_positions(curr_state.state[i][j]) #coordinates of current tile in goal board
+                manhattan_distance = abs(curr_pos[0] - goal_pos[0]) + abs(curr_pos[1] - goal_pos[1])
             
-            h2 += manhattan_distance
+                h2 += manhattan_distance
         
     curr_state.h = h2
 
@@ -57,25 +58,25 @@ if __name__ == '__main__':
         # print(initState.state)
         #Running a_star with both h1 and h2
         heuristics = [h1, h2]
-        
+
         h = 0
         for heuristic in heuristics:
             h +=1
             #Initializing PriorityQueue frontier with neighbors
             frontier = PriorityQueue()
-            
+
             frontier.put((0, initState))
-            
+
             empty = False
             goal_found = False
             nodes_visited = 0
-            
+
             #Establishing the goal state
             goal = eightpuzzle.EightPuzzle()
-            
+
             #Creating any empty set to keep track of visited nodes
             explored_set = set()
-                
+
             #Run a_star while frontier is full and goal hasn't been found
             while(not(empty) and not(goal_found)):
                 empty = frontier.empty()
@@ -83,7 +84,7 @@ if __name__ == '__main__':
                     break
                 if (nodes_visited > 0):
                     prev_node = next_node
-                    
+
                 next = frontier.get()
                 next_node = next[1]
                 #only add popped node to frontier if not yet explored
@@ -94,26 +95,26 @@ if __name__ == '__main__':
                         next_node = next[1]
                         if not(next_node in explored_set):
                             explored = False
-                        
+
                 #add non-eplored node to explored set
                 explored_set.add(next_node)
-                
+
                 if (nodes_visited == 0):
                     prev_node = next_node
-                
+
                 # print(next_node)
-                
+
                 nodes_visited += 1
                 if (next_node.state == goal.state):
                     goal_found = True
                     break
                 neighbors = next_node.neighbors()
                 for moves in neighbors:
-                    frontier.put((heuristic(moves[0], prev_node, goal) + moves[0].g, moves[0]))
-                
+                    frontier.put((heuristic(moves[0], goal) + moves[0].g, moves[0]))
+
             # if (empty):
                 # print("No possible solutions using", heuristic, "as the heuristic.")
-            
+
             if (goal_found):
                 # print("Found a solution using ", heuristic, "as the heuristic!")
                 # print("-----------------")
@@ -135,10 +136,18 @@ if __name__ == '__main__':
                         h2_branching.append(0)
                 # print("Depth: ", next_node.g)
                 # print("-----------------")
-    
+
 
     df = pandas.DataFrame(data={"h1 branching factor" : h1_branching, "h1 depth": h1_depth, "h1 nodes visited": h1_nodes, "h2 branching factor" : h2_branching, "h2 depth": h2_depth, "h2 nodes visited": h2_nodes})
-    df.to_csv("./file.csv", sep=',',index=False)
+    df.to_csv("./fixed_heuristics.csv", sep=',',index=False)
+    
+#    goal = eightpuzzle.EightPuzzle()
+#
+#    puzzle = eightpuzzle.EightPuzzle()
+#    puzzle.state = [[7,2,4],[5,0,6],[8,3,1]]
+#    h1 = h1(puzzle, goal)
+#    h2 = h2(puzzle, goal)
+#    print(h1, h2)
 
     # plt.plot(h1_depth, h1_nodes, 'r^', h2_depth, h2_nodes, 'bs')
     # plt.show()
